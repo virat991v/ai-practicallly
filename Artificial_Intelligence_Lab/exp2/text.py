03bb437a99612dbaf6ef44b2a086f963abf33f7b
virat991v <aryanvirat111@gmail.com>
2026-07-24 10:13:14 +0530

    python file is created

--- Artificial_Intelligence_Lab/exp2/text.py
+++ Artificial_Intelligence_Lab/exp2/text.py
@@ -1,16 +1,7 @@
-try:
-    num = int(input("Enter a positive integer: "))
+n = int(input("Enter a number: "))
+fact = 1
 
-    factorial = 1
-    if num < 0:
-        print("Factorial is not defined for negative numbers.")
-    elif num == 0:
-        print("The factorial of 0 is 1")
-    else:
-        for i in range(1, num + 1):
-            factorial *= i
-        
-        print(f"The factorial of {num} is {factorial}")
+for i in range(1, n + 1):
+    fact *= i
 
-except ValueError:
-    print("Invalid input! Please enter a valid whole number.")
+print("Factorial =", fact)
