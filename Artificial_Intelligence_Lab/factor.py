bb49885f77c46037d53b6450d0f3799c24f8e3f1
virat911v <aryanvirat111@gmail.com>
2026-08-24 22:04:45 +0530

    first practical of factor

---
+++ Artificial_Intelligence_Lab/factor.py
@@ -0,0 +1,20 @@
+a = float(input("Enter DS marks out of 100:"))
+b = float(input("Enter DELD marks out of 100:"))
+c = float(input("Enter OS marks out of 100:"))
+d = float(input("Enter marks of ENTP out off 100:"))
+e = float(input("Enter marks of UHV out off 100:"))
+
+Total = a + b + c + d + e
+per=Total/5
+
+
+if per >= 75:
+     print("Distinction")
+elif per < 75 and per >= 60:
+     print("First class")
+elif per < 60  and per >= 50:
+     print("second class")
+elif per <50  and per >= 35:
+     print("pass class")
+elif per <35  :
+     print("Fail")
